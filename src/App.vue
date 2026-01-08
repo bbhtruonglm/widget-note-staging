85fe6c24910768026dff5bf98323801685dbb752
bbhtruonglm <truonglm.bbh@gmail.com>
2026-01-08 13:50:39 +0700

    update commit

--- src/App.vue
+++ src/App.vue
@@ -8,114 +8,114 @@
 
 <script setup lang="ts">
 // * import function
-import { Toast } from '@/services/toast'
-import { queryString } from '@/services/helper'
-import { useAutoCreate } from '@/composable/useAutoCreate'
-import { useAppStore, useCommonStore } from '@/services/stores'
+import { Toast } from "@/services/toast";
+import { queryString } from "@/services/helper";
+import { useAutoCreate } from "@/composable/useAutoCreate";
+import { useAppStore, useCommonStore } from "@/services/stores";
 
 // * import library
-import WIDGET from 'bbh-chatbox-widget-js-sdk'
-import { onMounted, ref } from 'vue'
+import WIDGET from "bbh-chatbox-widget-js-sdk";
+import { onMounted, ref } from "vue";
 
 // * import component
-import DashBoard from '@/components/DashBoard.vue'
-import ActiveWidget from '@/components/ActiveWidget.vue'
-import Loading from '@/components/Loading.vue'
-import { set } from 'lodash'
+import DashBoard from "@/components/DashBoard.vue";
+import ActiveWidget from "@/components/ActiveWidget.vue";
+import Loading from "@/components/Loading.vue";
+import { set } from "lodash";
 
 // let url_string = window.location.href
 // let url = new URL(url_string)
 // globalThis.access_token = url.searchParams.get('access_token')
 
 // * store
-const appStore = useAppStore()
-const commonStore = useCommonStore()
+const appStore = useAppStore();
+const commonStore = useCommonStore();
 
 /** composable */
-const { listenPreviewEvent, sendPreviewEvent } = useAutoCreate()
+const { listenPreviewEvent, sendPreviewEvent } = useAutoCreate();
 
-// * khởi tạo hàm thông báo
-const $toast = new Toast()
+/** khởi tạo hàm thông báo */
+const $toast = new Toast();
 
 /** hàm check active widget */
-const active_app = ref<boolean>(false)
+const active_app = ref<boolean>(false);
 
 onMounted(() => {
   // hàm kiểm tra xem đã kích hoạt chưa và chuyển đến màn tương ứng
-  activeApp()
+  activeApp();
   // lắng nghe event từ merchant khi chuyển đoạn chat
   WIDGET.onEvent((e: any, data: any) => {
     /** dữ liệu tự động tạo ghi chú được gửi từ app chatbot Native */
-    const DATA = typeof data === 'string' ? JSON.parse(data) : data
-    
+    const DATA = typeof data === "string" ? JSON.parse(data) : data;
+
     // nếu sự kiện không phải là reload thì bỏ qua
-    if (DATA.type !== 'RELOAD' || DATA.from !== 'CHATBOX') return
+    if (DATA.type !== "RELOAD" || DATA.from !== "CHATBOX") return;
 
     // ghi lại thông tin khách hàng mới
-    getDataClient()
-  })
+    getDataClient();
+  });
 
   // gửi sự kiện đến chatbot để lấy dữ liệu
   sendPreviewEvent({
-    from: 'WIDGET_NOTE',
-    type: 'get.data'
-  })
+    from: "WIDGET_NOTE",
+    type: "get.data",
+  });
 
   // fakeEvent()
 
   // lắng nghe event từ app chatbot
   listenPreviewEvent((data: any) => {
     /** nội dung ghi chú */
-    const NOTE_CONTENT = data.note
+    const NOTE_CONTENT = data.note;
 
     /** thời gian hẹn lịch */
-    const DATE_TIME = data.datetime?.toString() || ''
+    const DATE_TIME = data.datetime?.toString() || "";
 
     /** đường dẫn nội dung ghi chú */
     const NOTE_PATH =
-      'public_profile.ai[0].ctas.schedule_appointment.input_message'
+      "public_profile.ai[0].ctas.schedule_appointment.input_message";
 
     /** đường dẫn thời gian hẹn lịch */
-    const DATE_PATH = 'public_profile.ai[0].ctas.schedule_appointment.datetime'
+    const DATE_PATH = "public_profile.ai[0].ctas.schedule_appointment.datetime";
 
     // lưu lại nội dung ghi chú
-    set(commonStore.data_client, NOTE_PATH, NOTE_CONTENT)
+    set(commonStore.data_client, NOTE_PATH, NOTE_CONTENT);
 
     // lưu lại thời gian hẹn lịch
-    set(commonStore.data_client, DATE_PATH, DATE_TIME)
+    set(commonStore.data_client, DATE_PATH, DATE_TIME);
 
     if (NOTE_CONTENT || DATE_TIME) {
-      appStore.tab_selected = 'CREATE_NEW'
-      appStore.is_auto_create = true
+      appStore.tab_selected = "CREATE_NEW";
+      appStore.is_auto_create = true;
     }
-  })
-})
+  });
+});
 
 /** hàm tự động tạo ghi chú */
 function autoCreate() {
-  let param_date = queryString('datetime') || ''
-  let note_content = queryString('note') || ''
+  let param_date = queryString("datetime") || "";
+  let note_content = queryString("note") || "";
 
-  const partner_token = queryString('partner_token')
+  const partner_token = queryString("partner_token");
 
   if (partner_token) {
     param_date =
       commonStore?.data_client?.public_profile?.ai?.[0]?.ctas?.schedule_appointment?.datetime?.toString() ||
-      ''
+      "";
     note_content =
       commonStore?.data_client?.public_profile?.ai?.[0]?.ctas
-        ?.schedule_appointment?.input_message || ''
+        ?.schedule_appointment?.input_message || "";
   }
 
-  if (!note_content && !param_date) return
+  if (!note_content && !param_date) return;
 
   /** hợp lệ của thời gian */
-  let is_date_valid = param_date
+  let is_date_valid = param_date;
 
   if (note_content || is_date_valid) {
     //chuyển màn tạo ghi chú
-    appStore.tab_selected = 'CREATE_NEW'
-    appStore.is_auto_create = true
+    appStore.tab_selected = "CREATE_NEW";
+    appStore.is_auto_create = true;
   }
 }
 
@@ -123,16 +123,16 @@ function autoCreate() {
 async function getDataClient() {
   try {
     //bật loading
-    commonStore.is_loading_full_screen = true
+    commonStore.is_loading_full_screen = true;
 
-    await decodeClient()
+    await decodeClient();
 
     //tắt loading
-    commonStore.is_loading_full_screen = false
+    commonStore.is_loading_full_screen = false;
   } catch (error) {
-    console.log('getDataClient', error)
+    console.log("getDataClient", error);
     //tắt loading
-    commonStore.is_loading_full_screen = false
+    commonStore.is_loading_full_screen = false;
   }
 }
 
@@ -140,32 +140,32 @@ async function getDataClient() {
 async function activeApp() {
   try {
     //bật loading
-    commonStore.is_loading_full_screen = true
+    commonStore.is_loading_full_screen = true;
 
-    await decodeClient()
+    await decodeClient();
 
     //nếu thành công thì không cho vào màn kích hoạt
-    active_app.value = false
+    active_app.value = false;
 
-    autoCreate()
+    autoCreate();
 
     //tắt loading
-    commonStore.is_loading_full_screen = false
+    commonStore.is_loading_full_screen = false;
   } catch (error) {
-    console.log('activeApp', error)
+    console.log("activeApp", error);
 
     // thông báo nếu scret key sai
-    if (error === 'WRONG_SECRET_KEY') {
+    if (error === "WRONG_SECRET_KEY") {
       $toast.error(
-        'Secret key của ghi chú sai, vui lòng liên hệ với kỹ thuật viên'
-      )
+        "Secret key của ghi chú sai, vui lòng liên hệ với kỹ thuật viên"
+      );
     }
 
     // nếu không lấy thành công thì chuyển sang màn kích hoạt
-    active_app.value = true
+    active_app.value = true;
 
     //tắt loading
-    commonStore.is_loading_full_screen = false
+    commonStore.is_loading_full_screen = false;
   }
 }
 
@@ -177,7 +177,7 @@ async function decodeClient() {
     // console.log('partner_token', partner_token);
 
     if (WIDGET.partner_token) {
-      commonStore.data_client = await WIDGET.getClientInfo()
+      commonStore.data_client = await WIDGET.getClientInfo();
       // const data = await decodeClientV2({
       //   access_token: queryString('partner_token'),
       //   client_id: queryString('client_id'),
@@ -186,11 +186,11 @@ async function decodeClient() {
       // })
       // commonStore.data_client = data.data
     } else {
-      commonStore.data_client = await WIDGET.decodeClient()
+      commonStore.data_client = await WIDGET.decodeClient();
     }
   } catch (error) {
-    throw error
-    console.log('getDataClient', error)
+    throw error;
+    console.log("getDataClient", error);
   }
 }
 </script>
